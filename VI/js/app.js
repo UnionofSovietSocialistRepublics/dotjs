dd956240d1957e6de9a16e91f7710e6bb6f2aa13
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2023-09-23 21:32:00 +0700

    Waltuh put your COC away waltuh

---
+++ VI/js/app.js
@@ -0,0 +1,42 @@
+
+var b = 1;
+//var is var
+let c = 1;
+//let is let
+const d = 9;
+//Const CANNOT be changed
+
+
+var age = 21;
+console.log("My age", age);
+
+//boolaelean
+var isAdult = true;
+console.log("You are adopted", isAdult);
+
+var firstname = "Adolf";
+console.log("Your name is ADO-", firstname);
+
+var phone = null;
+console.log("This is null.", phone);
+
+var defined = undefined;
+console.log("Undefine your define", defined);
+
+var books = ["books" , "books" ,"heavy", "Meinkramp"];
+console.log("your least favorite books are", books)
+
+var friend = {
+    full_name: "Aois Hitler",
+    hobby: "Your mother",
+    religion: "Jewish (very ironic)"
+};
+console.log(friend);
+
+//range
+//var global
+//let local(??)
+//const weed eater
+
+//if else: loop
+//very unfuni
